94bb2299f488c3fe766cc48f2e7b25c1dc21c13e
Bob Hunt <bob.hunt.email@gmail.com>
2018-12-03 11:12:07 -0500

    first 2 of 3 steps are completed.

--- copyspecial.py
+++ copyspecial.py
@@ -12,31 +12,48 @@ import os
 import shutil
 import commands
 import argparse
+import zipfile
 
 """Copy Special exercise
 """
 
 
 def get_special_paths(dir):
-    spec_paths = []
+    paths = []
     ### returns a list of the absolute paths of the special files in the given directory ###
-    for root, dirs, files in os.walk("."):  
+    # need to change to >>> os.listdir(dir):
+    for __, __, files in os.walk(dir):  
         for filename in files:
             if re.search(r'\__\w*\__', filename):
-                print(os.path.abspath(filename))
+                paths.append(os.path.abspath(filename))
+    return paths
 
-    return
 
-
-def copy_to(paths, dir):
-    ### given a list of paths, copies those files into the given directory ###
-    pass
+def copy_to(paths, todir):
+    if not os.path.isdir(todir):
+        os.makedirs(todir)
+    todir_path = os.path.abspath(todir)
+    for path in paths:
+        shutil.copy(path, todir_path)
+#     return is not needed
 
 
 def zip_to(paths, zippath):
     ### given a list of paths, zip those files up into the given zipfile ###
-    pass
+    print("I'm zippy!")
+    if not os.path.isfile(zippath):
+        print('Yep, it is a file!')
+        # zip_archive = zipfile.ZipFile(filename, "w")
+        # zip_archive.write('Testing 1,2,3.')
+        # zip_archive.close()
 
+    else:
+        print('Nope! NOT a file!')
+    
+    # with zipfile.ZipFile(filename, "w") as zf
+    # -or-
+    # zip_archive = zipfile.ZipFile(filename, "w")
+    # zip_archive.close()
 
 def create_parser():
     # This snippet will help you get started with the argparse module.
@@ -45,7 +62,7 @@ def create_parser():
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO need an argument to pick up 'from_dir'
     parser.add_argument(
-        '-d', '--dir', help='directory of files to search', default='.')
+        '-d', '--dir', help='source directory of files to search', default='.')
     return parser
 
 # Write functions and modify main() to call them
@@ -60,12 +77,24 @@ def main(args):
 
     print('my_args: {my_args}'.format(my_args=my_args))
 
-#  *** next 3 lines are from dotm_search ***
     path_to_search = my_args.dir
-    get_special_paths(dir)
+    dest_path = my_args.todir
+    zip_path = my_args.tozip
 
-    # text_to_find = my_args.text
-    # find_dotm(path_to_search, text_to_find)
+    # assign variable to the output of the first function 
+    my_paths = get_special_paths(path_to_search)
+
+    # Business logic: 
+    # Choose one of three ways to process the my_paths list,
+    # based on cmd line options present or not present
+    if dest_path:
+        copy_to(my_paths, dest_path)
+    elif zip_path:
+        zip_to(my_paths, zip_path)
+    else:
+        print('Here are the special files: {}'.format("\n".join(my_paths)))
+
+
 
     # TODO you must write your own code to get the cmdline args.
     # Read the docs and examples for the argparse module about how to do this.
@@ -74,9 +103,11 @@ def main(args):
     # This is input data validation.  If something is wrong (or missing) with any
     # required args, the general rule is to print a usage message and exit(1).
 
-    # +++your code here+++
     # Call your functions
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    # the following line shaves the program file name off of the command line args
+    my_args = sys.argv[1:]
+    print my_args
+    main(my_args)
